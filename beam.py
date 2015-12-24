0fd98d9a08f4aecab55d5a394e8651d26dcdd031
Prindl <maximilianprindl@hotmail.com>
2015-12-24 15:52:13 +0100

    Update beam.py

--- beam.py
+++ beam.py
@@ -33,58 +33,30 @@ module beam.py implements these methods:
 #math implements basic math functions: exp(), sqrt(), ln(), erf(),...
 import scipy.constants as constants
 import math
-import parameter as loadparameterfile
+import parameter as par
 
-#create global variables of parameters to be read in
-
-BEAM_CURRENT_DENSITY = None
-FWHM = None
-SCAN_WIDTH = None
-BEAM_CURRENT = None
-BEAM_CENTER = None
-ERF_BEAM_WIDTH = None
-BEAM_TYPE = None
-
-
-def init():
-    loadparameterfile.init()
-    parameterdict = loadparameterfile.toDict()
-    
-    #modify namespace to use global variables
-    global BEAM_CURRENT_DENSITY
-    global FWHM
-    global SCAN_WIDTH
-    global BEAM_CURRENT
-    global BEAM_CENTER
-    global ERF_BEAM_WIDTH
-    global BEAM_TYPE
-    
-    BEAM_CURRENT_DENSITY = float(parameterdict['BEAM_CURRENT_DENSITY'])
-    FWHM = float(parameterdict['FWHM'])
-    SCAN_WIDTH = float(parameterdict['SCAN_WIDTH'])
-    BEAM_CURRENT = float(parameterdict['BEAM_CURRENT'])
-    BEAM_CENTER = float(parameterdict['BEAM_CENTER'])
-    ERF_BEAM_WIDTH = float(parameterdict['ERF_BEAM_WIDTH'])
-    BEAM_TYPE = str(parameterdict['BEAM_TYPE'])
 
 def sigma_by_fwhm():
-    return FWHM/(math.sqrt(8 * math.log(2)))
+    return par.FWHM/(math.sqrt(8 * math.log(2)))
     
 def get_beam(x):
-    if BEAM_TYPE == 'constant':
+    if par.BEAM_TYPE == 'constant':
         return homogenous_beam()
-    elif BEAM_TYPE == 'gaussian':
+    elif par.BEAM_TYPE == 'gaussian':
         return gaussian_beam(x)
-    elif BEAM_TYPE == 'error function':
+    elif par.BEAM_TYPE == 'error function':
         return erf_beam(x)
     
-def gaussian_beam(x):
-    return ((BEAM_CURRENT/constants.e)/(math.sqrt(2*constants.pi) * sigma_by_fwhm() * SCAN_WIDTH)) * math.exp(-((x - BEAM_CENTER)**2)/(2*(sigma_by_fwhm()**2))) 
-
+def gaussian_beam(xvals):
+    beam= []
+    for x in xvals:
+        beam.append( ((par.BEAM_CURRENT/constants.e)/(math.sqrt(2*constants.pi) * sigma_by_fwhm() * par.SCAN_WIDTH)) * math.exp(-((x - par.BEAM_CENTER)**2)/(2*(sigma_by_fwhm()**2))) )
+    return beam    
+    
 def erf_beam(x):
-    x1 = BEAM_CENTER - ERF_BEAM_WIDTH/2
-    x2 = BEAM_CENTER + ERF_BEAM_WIDTH/2
-    return ((BEAM_CURRENT/constants.e)/(2*SCAN_WIDTH*ERF_BEAM_WIDTH)) *(math.erf(-(x-x2)/(math.sqrt(2) * sigma_by_fwhm())) - math.erf(-(x-x1)/(math.sqrt(2) * sigma_by_fwhm())))
+    x1 = par.BEAM_CENTER - par.ERF_BEAM_WIDTH/2
+    x2 = par.BEAM_CENTER + par.ERF_BEAM_WIDTH/2
+    return ((par.BEAM_CURRENT/constants.e)/(2*par.SCAN_WIDTH*par.ERF_BEAM_WIDTH)) *(math.erf(-(x-x2)/(math.sqrt(2) * sigma_by_fwhm())) - math.erf(-(x-x1)/(math.sqrt(2) * sigma_by_fwhm())))
 
 def homogenous_beam():
-    return BEAM_CURRENT_DENSITY/constants.e
+    return par.BEAM_CURRENT_DENSITY/constants.e
