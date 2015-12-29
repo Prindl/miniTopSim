f60a43ff15a928e63697692a4fe9ae969b9f822f
Prindl <maximilianprindl@hotmail.com>
2015-12-29 18:42:49 +0100

    Update beam.py
    Removed for-Loops, uses whole array operations instead.

--- beam.py
+++ beam.py
@@ -3,63 +3,53 @@
 This module implements three variants of ion beams.
 Beam type depends on parameter read from config file:
 [BEAM] BEAM_TYPE
+
 constant: homogen beam
-parameter:
-    -[BEAM] BEAM_CURRENT_DENSITY
 gaussian: beam is presented by gaussian distrubution
-parameter:
-    -[BEAM] BEAM_CURRENT
-    -[BEAM] SCAN_WIDTH
-    -[BEAM] BEAM_CENTER
-    -[BEAM] FWHM
 error_function: approximation of gaussian beam by two error functions
-parameter:
-    -[BEAM] BEAM_CURRENT
-    -[BEAM] SCAN_WIDTH
-    -[BEAM] BEAM_CENTER
-    -[BEAM] FWHM
-    -[BEAM] ERF_BEAM_WIDTH
-    
+
 module beam.py implements these methods:
-    init()
-        initializes the parameter.py module and loads parameters
-    get_beam(x)
-        calculates the beam at x according to set parameters
-    
+
+    get_beam(template)
+        calculates the beam at all x positions
+		
+    sigma_by_fwhm()
+        returns the standard deviation calculated by fwhm
 """
 
-#import needed modules
-#scipy.constants provides constants: c0, e, µ0,...
-#math implements basic math functions: exp(), sqrt(), ln(), erf(),...
-import scipy.constants as constants
+'''
+	import needed modules
+	scipy.constants provides constants: c0, e, µ0,...
+	math implements basic math functions: exp(), sqrt(), ln(), erf(),...
+	scipy.special.erf method provides a method for calculating erf with numpy arrays
+'''
 import math
 import parameter as par
+import numpy as np
+from scipy.special import erf
+from scipy.constants import pi, e
 
-
-def sigma_by_fwhm():
-    return par.FWHM/(math.sqrt(8 * math.log(2)))
     
-def get_beam(x):
-    if par.BEAM_TYPE == 'constant':
-        return homogenous_beam()
-    elif par.BEAM_TYPE == 'gaussian':
-        return gaussian_beam(x)
-    elif par.BEAM_TYPE == 'error function':
-        return erf_beam(x)
+'''
+	depending on the parameter BEAM_TYPE a beam function is selected
+'''
+def get_beam(xvals):
+    sigma = par.FWHM/(math.sqrt(8 * math.log(2))) #standard deviation
+    x1 = np.full_like(xvals, par.BEAM_CENTER - par.ERF_BEAM_WIDTH/2)
+    x2 = np.full_like(xvals, par.BEAM_CENTER + par.ERF_BEAM_WIDTH/2)    
+    try:
+        if par.BEAM_TYPE == 'constant':
+            return par.BEAM_CURRENT_DENSITY/e
+            
+        elif par.BEAM_TYPE == 'gaussian':
+            return (par.BEAM_CURRENT/e)/(math.sqrt(2*pi) * sigma * par.SCAN_WIDTH) * np.exp(-((xvals - np.full_like(xvals, par.BEAM_CENTER))**2)/(2*(sigma**2)))
+        
+        elif par.BEAM_TYPE == 'error function':
+            return ((par.BEAM_CURRENT/e)/(2*par.SCAN_WIDTH*par.ERF_BEAM_WIDTH)) * (erf(-(xvals-x2)/(math.sqrt(2) * sigma)) - erf(-(xvals-x1)/(math.sqrt(2) * sigma)))
+        
+        else:
+            print('error: beam type could not be identified')
+            exit(-1)
+    except ZeroDivisionError as ZDE:
+        print(ZDE.args)
     
-def gaussian_beam(xvals):
-    beam= []
-    for x in xvals:
-        beam.append( ((par.BEAM_CURRENT/constants.e)/(math.sqrt(2*constants.pi) * sigma_by_fwhm() * par.SCAN_WIDTH)) * math.exp(-((x - par.BEAM_CENTER)**2)/(2*(sigma_by_fwhm()**2))) )
-    return beam    
-    
-def erf_beam(xvals):
-    beam= []
-    x1 = par.BEAM_CENTER - par.ERF_BEAM_WIDTH/2
-    x2 = par.BEAM_CENTER + par.ERF_BEAM_WIDTH/2
-    for x in xvals:
-        beam.append(((par.BEAM_CURRENT/constants.e)/(2*par.SCAN_WIDTH*par.ERF_BEAM_WIDTH)) *(math.erf(-(x-x2)/(math.sqrt(2) * sigma_by_fwhm())) - math.erf(-(x-x1)/(math.sqrt(2) * sigma_by_fwhm()))) )
-    return beam
-    
-def homogenous_beam():
-    return par.BEAM_CURRENT_DENSITY/constants.e
